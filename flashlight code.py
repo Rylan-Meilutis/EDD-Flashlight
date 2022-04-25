4e39f7205a137bb1dc9b5b823c219783079ffc29
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-04-25 08:05:15 -0400

    it works now

--- flashlight code.py
+++ flashlight code.py
@@ -2,6 +2,7 @@ import time
 from threading import Thread
 
 import RPi.GPIO as GPIO
+import platform
 
 GPIO.setmode(GPIO.BOARD)
 button_id = 16
@@ -11,16 +12,25 @@ GPIO.setup(led, GPIO.OUT)
 state = 0
 do_blink = False
 timed_out = True
+is_raspberry = True if platform.uname()[4] != "x86_64" and platform.uname()[4] != "AMD64" else False
+
+keystroke = False
 
 
 def get_button():
-    global state
-    button = GPIO.event_detected(button_id)
+    global state, keystroke
+    if is_raspberry:
+        button = keystroke
+    else:
+        button = GPIO.event_detected(button_id)
+    # print(button, end='\r')
     if button:
+        if is_raspberry:
+            keystroke = False
         if timed_out:
             thr = Thread(target=click, daemon=True)
             thr.start()
-            if state != 0:
+            if state == 0:
                 state = 1
             else:
                 state = 0
@@ -39,10 +49,12 @@ def click():
 
 
 def blink():
+    global do_blink
     while True:
         if do_blink:
             GPIO.output(led, GPIO.HIGH)
-            time.sleep(0.2)
+            print("blink")
+            time.sleep(0.002)
             GPIO.output(led, GPIO.LOW)
 
 
@@ -52,17 +64,31 @@ def main():
     t1.start()
     GPIO.add_event_detect(button_id, GPIO.RISING)
     while True:
+        # print(keystroke)
         get_button()
 
         if state == 0:
             do_blink = False
             GPIO.output(led, GPIO.LOW)
+            print("off", end='\r')
         elif state == 1:
             do_blink = False
             GPIO.output(led, GPIO.HIGH)
+            print("on", end='\r')
         else:
             do_blink = True
 
 
+def tester():
+    global keystroke
+    while True:
+        input()
+        keystroke = True
+
+
 if __name__ == "__main__":
+    if is_raspberry:
+        keys = Thread(target=tester, daemon=True)
+        keys.start()
     main()
+
