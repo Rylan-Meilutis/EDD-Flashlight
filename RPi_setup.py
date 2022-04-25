170946e4c95b67e66154af0992ddb413a3293f2d
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-04-25 07:46:03 -0400

    made RPi setup more reliable

--- RPi_setup.py
+++ RPi_setup.py
@@ -1,8 +1,14 @@
 import os
+import platform
 
 
 def main():
-    os.system("pip install RPi-1.0-py2.py3-none-any.whl")
+    if platform.system() == "Windows":
+        os.system("pip install RPi-1.0-py2.py3-none-any.whl")
+    elif platform.system() == "Linux":
+        os.system("pip3 install RPi-1.0-py2.py3-none-any.whl")
+    else:
+        print("This script hasn't been tested on your os, please install the package manually")
 
 
 if __name__ == "__main__":
