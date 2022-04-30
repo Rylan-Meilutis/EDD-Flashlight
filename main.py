1b9fc02f4fa26b8cbbcd6402909b69b5a4eac487
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-04-29 22:36:01 -0400

    added code that kind of works for the pi pico

---
+++ main.py
@@ -0,0 +1,28 @@
+from machine import ADC, Pin
+
+high_level = False
+button = Pin(5, Pin.IN, Pin.PULL_UP)
+press = False
+switch = False
+
+
+def toggle():
+    global switch, button, press, high_level
+    press = True if button.value() == 0 else False
+    if press:
+        if not switch:
+            high_level = not high_level
+        switch = True
+
+    else:
+        switch = False
+
+
+led = Pin(20, Pin.OUT)
+
+while True:
+    toggle()
+    if high_level == 0:
+        led.value(1)
+    else:
+        led.value(0)
